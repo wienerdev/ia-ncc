ab176efe9374934ed4ecd67185b33e45459ca068
wienerdev <matheuswiener9@gmail.com>
2021-10-13 15:29:03 -0300

    Separated pages with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
-import {useState} from "react";
+import { useState } from "react";
 import Topbar from "./components/topbar/Topbar";
 import "./app.scss"
-import HomePage from "./components/home/HomePage";
 import Menu from "./components/menu/Menu";
+import Concepts from "./components/home/concepts/Concepts"
+import Codes from "./components/home/codes/Codes"
+import Intro from "./components/home/intro/Intro";
+import Subscription from "./components/home/subscription/Subscription";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 
+
 function App() {
-  const [menuOpen,setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
 
   return (
     <Router>
       <div className="app">
-      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <Switch>
-        <Route path="/" exact>
-          <HomePage/>
-        </Route>
-      </Switch>
-    </div>
+        <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Switch>
+          <Route path="/" exact>
+            <Intro />
+          </Route>
+          <Route path="/concepts">
+            <Concepts />
+          </Route>
+          <Route path="/codes">
+            <Codes />
+          </Route>
+        </Switch>
+        <Subscription />
+      </div>
     </Router>
   );
 }
