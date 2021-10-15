675325923e7b04d5a36798683be5f61e8734019c
wienerdev <matheuswiener9@gmail.com>
2021-10-15 19:06:00 -0300

    Footer changed

--- src/components/footbar/Footbar.jsx
+++ src/components/footbar/Footbar.jsx
@@ -9,37 +9,33 @@ export default function Footbar() {
         <div className="footbar">
             <div className="image">
                 <div className="logo">
-                    <a href="https://www.uniceub.br/">
-                        <img src="assets/logoCEUB2021.png" alt="UniCEUB" />
+                    <a href="https://forms.gle/h6qAFNfrv9ePnENBA">
+                        <img src="assets/logo.png" alt="UniCEUB" />
                     </a>
                 </div>
                 <div className="social">
-                    <a href="https://www.facebook.com/uniceub">
+
+                    <a href="https://www.facebook.com/prof.aderbal">
                         <FontAwesomeIcon className="social-icon" icon={faFacebook} color="white" />
                     </a>
-                    <a href="https://twitter.com/uniceub">
+                    <a href="https://twitter.com/aderbalbotelho">
                         <FontAwesomeIcon className="social-icon" icon={faTwitter} color="white" />
                     </a>
-                    <a href="https://www.linkedin.com/school/uniceub-centro-universitario-de-brasilia/?originalSubdomain=pt">
+                    <a href="https://www.linkedin.com/in/aderbalbotelho/">
                         <FontAwesomeIcon className="social-icon" icon={faLinkedin} color="white" />
                     </a>
-                    <a href="http://instagram.com/uniceub_oficial">
+                    <a href="hhttps://www.instagram.com/aderbalbotelho/">
                         <FontAwesomeIcon className="social-icon" icon={faInstagram} color="white" />
                     </a>
-                    <a href="https://www.youtube.com/user/uniceub">
+                    <a href="https://www.youtube.com/channel/UCmH7uiBtICUMmAKUG1ipt6Q">
                         <FontAwesomeIcon className="social-icon" icon={faYoutube} color="white" />
                     </a>
                 </div>
             </div>
             <div className="text">
                 <div className="left">
-                    <span>Central de Atendimento: (61) 3966-1201 - central.atendimento@uniceub.br</span>
-                    <span>Unidades</span>
-                    <span>Campus da Asa Norte: SEPN 707/907 - Campus Universitário</span>
-                    <span>Campus II de Taguatinga: QS 1, Lote 1/17, Rua 214</span>
-                    <span>Centro de Atendimento à Comunidade: Setor Comercial Sul, Quadra 1 - Edificio União, Brasília-DF</span>
-                </div>
-                <div className="right">
+                    <h2>Projetos</h2>
+                    <div className="bar"></div>
                     <a href="https://www.ceubos.com.br" target="_blank" rel="noreferrer">CeubOS</a>
                     <a href="https://ctf.ceubos.com.br" target="_blank" rel="noreferrer">Competições CTF</a>
                     <a href="https://boca.ceubos.com.br/boca" target="_blank" rel="noreferrer">Treino ICPC</a>
@@ -47,6 +43,12 @@ export default function Footbar() {
                     <a href="https://jenkins.ceubos.com.br/" target="_blank" rel="noreferrer">Automatizador de processos</a>
                     <a href="https://koop.ceubos.com.br/" target="_blank" rel="noreferrer">Projetos colaborativos</a>
                     <a href="https://jupyter.ceubos.com.br/" target="_blank" rel="noreferrer">Jupyter Notebook</a>
+                </div>
+                <div className="center">
+
+                </div>
+                <div className="right">
+
                 </div>
             </div>
         </div>
